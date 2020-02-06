1c8346bd5fef63ac207a4e780ba19494166969f2
ziyonghong <1131002466@qq.com>
2020-02-06 16:15:03 +0800

    保存输入框内容到文件

--- app/src/main/java/com/example/filepersistencetest/MainActivity.java
+++ app/src/main/java/com/example/filepersistencetest/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.filepersistencetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+//在输入数据被回收前，将它存储到文件中
 public class MainActivity extends AppCompatActivity {
-
+    private EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit= (EditText) findViewById(R.id.edit);
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        String inputText=edit.getText().toString();
+        save(inputText);
+    }
+
+    public void save(String inputText){
+        FileOutputStream out=null;
+        BufferedWriter writer=null;
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer=new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
